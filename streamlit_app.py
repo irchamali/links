c0ff34b2d302b237ccc67e5c94abc4f756cc7e65
irchamali <id.irchamali@gmail.com>
2023-05-03 08:23:08 +0700

    Update link on social media and add github star

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,19 +4,21 @@ from PIL import Image
 
 load_css()
 
+st.write("[![Star](https://img.shields.io/github/stars/irchamali/links.svg?logo=github&style=social)](https://github.com/irchamali/links)")
+
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('ia.png'))
 
-st.header('Ircham Ali, M.Kom')
+st.header('Ircham Ali, M.Kom.')
 
-st.info('Web Developer and Informatics Lecturer')
+st.info('System Analyst and Informatics Lecturer')
 
 icon_size = 20
 
-st_button('youtube', 'https://www.youtube.com/channel/UCsqZalj5sdMDX2yUoeLMkXw', 'Unusia Labs', icon_size)
-st_button('medium', 'https://broirham.medium.com/', 'Read my Blogs', icon_size)
-st_button('instagram', 'https://instagram.com/bro_irham/', 'Folloe me on Instagram', icon_size)
-st_button('twitter', 'https://twitter.com/bro_irham/', 'Follow me on Twitter', icon_size)
-st_button('facebook', 'https://facebook.com/dosenirham/', 'Follow me on Facebook', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/ircham-ali/', 'Follow me on LinkedIn', icon_size)
+st_button('youtube', 'https://www.youtube.com/@unusialabs', 'Unusia Labs', icon_size)
+st_button('medium', 'https://irchamali.medium.com/', 'Read my Blogs', icon_size)
+st_button('instagram', 'https://instagram.com/irchamali_/', 'Follow me on Instagram', icon_size)
+st_button('twitter', 'https://twitter.com/irchamxyz/', 'Follow me on Twitter', icon_size)
+st_button('facebook', 'https://facebook.com/dosenirham/', 'Follow me on Facebook', icon_size)
 
