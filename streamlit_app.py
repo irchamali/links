3aa1e8974c6bdfac4c816e71ca5b4bdd73afe88e
irchamali <id.irchamali@gmail.com>
2023-05-03 09:06:01 +0700

    Update profile and social links

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,20 @@ from PIL import Image
 
 load_css()
 
-st.write("[![Star](https://img.shields.io/github/stars/irchamali/links.svg?logo=github&style=social)](https://github.com/irchamali/links)")
+st.write("[![Star](https://img.shields.io/github/stars/irchamali.svg?logo=github&style=social)](https://github.com/irchamali)")
 
 col1, col2, col3 = st.columns(3)
 col2.image(Image.open('ia.png'))
 
 st.header('Ircham Ali, M.Kom.')
 
-st.info('System Analyst and Informatics Lecturer')
+st.info('System Analyst, Web Designer, and Informatics Lecturer')
 
 icon_size = 20
 
 st_button('linkedin', 'https://www.linkedin.com/in/ircham-ali/', 'Follow me on LinkedIn', icon_size)
-st_button('youtube', 'https://www.youtube.com/@unusialabs', 'Unusia Labs', icon_size)
-st_button('medium', 'https://irchamali.medium.com/', 'Read my Blogs', icon_size)
+st_button('youtube', 'https://www.youtube.com/@unusialabs', 'Subscribe our YouTube', icon_size)
+st_button('medium', 'https://irchamali.medium.com/', 'Read my Medium Blogs', icon_size)
 st_button('instagram', 'https://instagram.com/irchamali_/', 'Follow me on Instagram', icon_size)
 st_button('twitter', 'https://twitter.com/irchamxyz/', 'Follow me on Twitter', icon_size)
 st_button('facebook', 'https://facebook.com/dosenirham/', 'Follow me on Facebook', icon_size)
