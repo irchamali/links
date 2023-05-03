cbcc149c61934ac60e01fd36676adb8dbdac1ff6
irchamali <id.irchamali@gmail.com>
2023-05-03 10:04:53 +0700

    Update function for github button

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,9 @@ st.info('System Analyst, Web Designer, and Informatics Lecturer')
 icon_size = 20
 
 st_button('linkedin', 'https://www.linkedin.com/in/ircham-ali/', 'Follow me on LinkedIn', icon_size)
-st_button('youtube', 'https://www.youtube.com/@unusialabs', 'Subscribe our YouTube', icon_size)
+st_button('youtube', 'https://www.youtube.com/@unusialabs', 'Unusia Labs YouTube', icon_size)
 st_button('medium', 'https://irchamali.medium.com/', 'Read my Medium Blogs', icon_size)
 st_button('instagram', 'https://instagram.com/irchamali_/', 'Follow me on Instagram', icon_size)
 st_button('twitter', 'https://twitter.com/irchamxyz/', 'Follow me on Twitter', icon_size)
 st_button('facebook', 'https://facebook.com/dosenirham/', 'Follow me on Facebook', icon_size)
-
+st_button('github', 'https://github.com/irchamali/', 'Follow me on GitHub', icon_size)
